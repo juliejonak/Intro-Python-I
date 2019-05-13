3d53c4dc599ed36b6e8f8605a034514c4c72f814
Julie Jonak <juliejonak10@gmail.com>
2019-05-13 10:17:06 -0700

    Completes cal.py

--- src/cal.py
+++ src/cal.py
@@ -19,8 +19,38 @@ and does the following:
    Then exit the program.
 """
 
+# Datetime examples: https://www.pythonforbeginners.com/basics/python-datetime-time-examples
+
 import sys
 import calendar
-from datetime import datetime
+# "from datetime import datetime" only imports the datetime class of datetime, making it incapalbe of using .date.today() and other methods
+# best to import datetime completed; but could also do: from datetime import datetime, date
+import datetime
+import time
+# How to use time and datetime: https://www.programiz.com/python-programming/datetime/strftime
 
-def getDate:
+def getDate(*dates):
+  year = int(datetime.date.today().strftime('%Y'))
+  month = int(datetime.date.today().strftime('%m'))
+
+  if(len(dates) == 0):
+    # return current month
+    print('current month: ', datetime.date.today())
+    calendar.prmonth(year, month)
+  
+  elif(len(dates) == 1):
+    # return month specified
+    calendar.prmonth(year, dates[0])
+  
+  elif(len(dates) == 2):
+    # return month and year specified
+    calendar.prmonth(dates[1], dates[0])
+  
+  else:
+    # return proper syntax
+    print('Please enter a valid month (1-12), optionally followed by a valid year.')
+  
+getDate()
+getDate(12)
+getDate(2, 2015)
+getDate(8, 2009, 0)
